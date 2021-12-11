e8cbec93cc281fdc3249b1c260eea8265f400507
unknown <daoudwissal2000@gmail.com>
2021-12-11 15:51:39 +0100

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,88 @@
+//const Scene = require('Scene');
+const Diagnostics = require('Diagnostics');
+const Materials = require('Materials');
+const Textures = require('Textures');
+const Patches = require('Patches');
+
+//const text = Scene.root.findFirst('angry_part');
+var face = Patches.outputs.getBoolean('angry'); 
+
+//Patches.inputs.setString('toScriptValue',);
+ Patches.outputs.getString('emoji')
+Patches.outputs.getPulse('PulseBlink').finally(function(e){
+//this will run each timethe user blinkss
+ });
+ (async function() { // Enable async/await in JS [part 1]
+
+  // Create a boolean variable
+  const myBoolean = true;
+  
+  const Text = Patches.outputs.getString('Text'); 
+  const emoji = 'Angry' 
+
+await Patches.inputs.setString('emoji',await emoji);
+
+  // Send the boolean to the Patch Editor as 'myBoolean'
+ // await Patches.inputs.setString('Text',await stringSignal1);
+})();
+//const text = Scene.root.findFirst('smile-part');
+var face1 = Patches.outputs.getBoolean('smile'); 
+
+//Patches.inputs.setString('toScriptValue',);
+ Patches.outputs.getString('emoji1')
+Patches.outputs.getPulse('PulseBlink').finally(function(e){
+//this will run each timethe user blinkss
+ });
+ (async function() { // Enable async/await in JS [part 1]
+
+  // Create a boolean variable
+  const myBoolean = true;
+  
+  const Text1 = Patches.outputs.getString('Text1'); 
+  const emoji1 = 'Smile' 
+
+await Patches.inputs.setString('emoji1',await emoji1);
+
+  // Send the boolean to the Patch Editor as 'myBoolean'
+ // await Patches.inputs.setString('Text',await stringSignal1);
+})();
+var face2 = Patches.outputs.getBoolean('Surprised'); 
+
+//Patches.inputs.setString('toScriptValue',);
+ Patches.outputs.getString('emoji2')
+Patches.outputs.getPulse('PulseBlink').finally(function(e){
+//this will run each timethe user blinkss
+ });
+ (async function() { // Enable async/await in JS [part 1]
+
+  // Create a boolean variable
+  const myBoolean = true;
+  
+  const Text2 = Patches.outputs.getString('Text2'); 
+  const emoji2 = 'Surprised' 
+
+await Patches.inputs.setString('emoji2',await emoji2);
+
+  // Send the boolean to the Patch Editor as 'myBoolean'
+ // await Patches.inputs.setString('Text',await stringSignal1);
+})();
+var face3 = Patches.outputs.getBoolean('Kiss'); 
+
+//Patches.inputs.setString('toScriptValue',);
+ Patches.outputs.getString('emoji3')
+Patches.outputs.getPulse('PulseBlink').finally(function(e){
+//this will run each timethe user blinkss
+ });
+ (async function() { // Enable async/await in JS [part 1]
+
+  // Create a boolean variable
+  const myBoolean = true;
+  
+  const Text3 = Patches.outputs.getString('Text3'); 
+  const emoji3 = 'Kiss' 
+
+await Patches.inputs.setString('emoji3',await emoji3);
+
+  // Send the boolean to the Patch Editor as 'myBoolean'
+ // await Patches.inputs.setString('Text',await stringSignal1);
+})();
